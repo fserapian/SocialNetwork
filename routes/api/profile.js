1b4b27378575cb754037d0b998a3b48dbda3cc86
Fadi Serapian <fadi.serapian@gmail.com>
2020-08-21 12:42:54 -0400

    add route in auth to get logged in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Profile route');
+/**
+ * @desc    Get logged in user profile
+ * @route   GET /api/profile/me
+ * @access  private
+ *
+ * @param {Object} req
+ * @param {Object} res
+ */
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    if (!profile) {
+      return res.status(404).json({
+        success: false,
+        error: 'No profile found for this user',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: profile,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 });
 
 module.exports = router;
