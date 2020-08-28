ab666a72d680eed7eda0ff381731b8978bd66ac7
Fadi Serapian <fadi.serapian@gmail.com>
2020-08-28 13:03:38 -0400

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+  <Router>
+    <Fragment>
       <Navbar />
-      <Landing />
-    </div>
-  );
-}
+      <Route exact path="/" component={Landing} />
+    </Fragment>
+  </Router>
+);
 
 export default App;
