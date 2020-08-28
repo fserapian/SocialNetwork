d4ba040153510fbe45c2775fcb3d4bd40784da8a
Fadi Serapian <fadi.serapian@gmail.com>
2020-08-28 11:53:13 -0400

    add client react app and update .gitignore

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl">Front-end app</h1>
+    </div>
+  );
+}
+
+export default App;
