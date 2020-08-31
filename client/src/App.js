80aa83d273bf090b8307a6d473ae78206294d7fe
Fadi Serapian <fadi.serapian@gmail.com>
2020-08-31 11:53:24 -0400

    add redux store to client

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,29 @@ import { Landing } from './components/layout/Landing';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <div className="container mx-auto px-4">
-        <Route exact path="/" component={Landing} />
-      </div>
-      <div className="grid grid-cols-8">
-        <div className="col-start-3 col-span-4">
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container mx-auto px-4">
+          <Route exact path="/" component={Landing} />
         </div>
-      </div>
-    </Fragment>
-  </Router>
+        <div className="grid grid-cols-8">
+          <div className="col-start-3 col-span-4">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
